337e61d19a8c9ad26b12ee54ffdb0d58f4daf443
milandashara <student1@CR33S29I.local>
2014-04-19 12:20:43 +0800

    gycyt

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by student on 4/19/14.
+//  Copyright (c) 2014 ISS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
