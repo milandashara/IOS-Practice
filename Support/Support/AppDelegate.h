3437dd4606ff295a458b18a79c7927df7a8b1626
student <student@DR28S08.local>
2014-04-24 14:20:03 +0800

    support

---
+++ Support/Support/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Support
+//
+//  Created by student on 24/4/14.
+//  Copyright (c) 2014 Milan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Support/Support/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Support
+//
+//  Created by student on 24/4/14.
+//  Copyright (c) 2014 Milan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
